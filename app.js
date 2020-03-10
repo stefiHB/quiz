cfd432df6b2a1af5dc3d3a7b7586e3cabf33baa8
stefihb <kassinopoulou.stephane@gmail.com>
2020-03-10 13:17:18 +0200

    Creating the possible answers

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 
 
-document.getElementById('start-btn').addEventListener('click', startQuiz);
-
-var quiz_title = document.getElementById('quiz-title');
-var quiz_description = document.getElementById('quiz-description');
-var question_numbering = document.getElementById('question-numbering');
+var quiz_title          = document.getElementById('quiz-title');
+var quiz_description    = document.getElementById('quiz-description');
+var question_numbering  = document.getElementById('question-numbering');
+var question_container  = document.getElementById('question-container');
+var answers_div         = document.createElement('div')
 
 let questions, index;
+
+document.getElementById('start-btn').addEventListener('click', startQuiz);
 
 function startQuiz() {
     fetchData().then(questions => {
@@ -42,6 +44,45 @@ function fetchData() {
     })
 }
 
+// create DOM elements for the question
 function displayQuestion() {
-    
+    // Create necessary elements 
+    let cur_question = questions[index];
+    let q_title = document.createElement("h2");
+    let q_img = document.createElement("img")
+    // Declare value of elements
+    question_numbering.innerText = `Question #${cur_question.q_id} out of ${questions.length}`
+    q_title.innerText = cur_question.title;
+    q_img.src = cur_question.img;
+    createPossibleAnswers(cur_question);
+
+    // Append elements on DOM
+    question_container.appendChild(q_title);
+    question_container.appendChild(q_img);
+    question_container.appendChild(answers_div);    
+}
+
+function createPossibleAnswers(question) {
+    let poss_answers = question.possible_answers
+    console.log('Create ', question.question_type);
+    switch(question.question_type) {
+        case 'truefalse':
+            trueFalse();
+            break;
+        case 'mutiplechoice-single':
+            console.log('in case');
+            multipleChoiceSingle(poss_answers);
+            break;
+        case 'mutiplechoice-multiple':
+            multipleChoiceMultiple(poss_answers);
+            break;
+    }
+}
+
+function multipleChoiceSingle(poss_answers) {
+
+    let multipleChoiceSingle_div = document.createElement('div');
+    multipleChoiceSingle_div.innerHTML = '<h1>TEST</h1>';
+    answers_div.appendChild(multipleChoiceSingle_div)
+
 }
